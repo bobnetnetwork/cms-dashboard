bcd202d6d177812e45d811960bcbaa0e937201e4
bobesz <bob@bobnet.network>
2020-09-05 19:15:46 +0200

    created articles routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticleComponent } from './article/article.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'articles', component: ArticleComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
