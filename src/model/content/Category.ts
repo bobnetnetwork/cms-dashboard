63253ac297ee08beeb3dee9fb1b304deb6af2307
bobesz <bob@bobnet.network>
2020-09-05 17:30:24 +0200

    Article, Category, Tag, Role, Users model added to dashboard

---
+++ src/model/content/Category.ts
@@ -0,0 +1,12 @@
+import { Observable } from 'rxjs';
+import { Article } from './Article';
+
+export class Category {
+  name?: string
+  description?: string
+  featuredImage?: string
+  slug?: string
+  addedAt?: Date
+  parent?: Category
+  articles?: Observable<Article>
+}
