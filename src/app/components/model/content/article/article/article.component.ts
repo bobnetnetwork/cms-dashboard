a4561e8216ccf8cfddb10663462a88054d8eb3d6
bobesz <bob@bobnet.network>
2020-09-07 20:49:15 +0200

    updated article editor page

--- src/app/components/model/content/article/article/article.component.ts
+++ src/app/components/model/content/article/article/article.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Article } from 'src/app/model/content/Article';
 import { ArticleService } from 'src/app/services/model/content/article/article.service';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 export class ArticleMessage {
   content: Article[];
@@ -18,13 +19,24 @@ export class ArticleComponent implements OnInit {
   private slug: string;
   private msg: ArticleMessage;
   article: Article;
+  articleForm: FormGroup;
 
-  constructor(private route: ActivatedRoute, private service: ArticleService) {
+  constructor(private route: ActivatedRoute, private service: ArticleService, private formBuilder: FormBuilder) {
+
     this.slug = this.route.snapshot.paramMap.get('articleslug');
     this.service.getArticle(this.slug).subscribe(res => {
       console.log(res);
       this.msg = res as ArticleMessage;
       this.article = this.msg.content as Article;
+      this.generateForm();
+    });
+
+  }
+
+  private generateForm() {
+    this.articleForm = new FormGroup({
+      title: new FormControl(this.article.title),
+      content: new FormControl(this.article.content)
     });
   }
 
@@ -32,4 +44,23 @@ export class ArticleComponent implements OnInit {
 
   }
 
+  saveAsDraft() {
+    console.log("Saved Draft!");
+    console.log(this.articleForm);
+    console.log("Title: ", this.articleForm.get("title").value);
+  }
+
+  publish() {
+    this.article.title = this.articleForm.get("title").value;
+    this.article.content = this.articleForm.get("content").value
+    this.service.updateArticle(this.article).subscribe(res => {
+      console.log("Update log: ", res);
+    });
+  }
+
+  delete() {
+    this.service.deleteArticle(this.article.slug).subscribe(res => {
+      console.log("Delete log: ", res);
+    });
+  }
 }
