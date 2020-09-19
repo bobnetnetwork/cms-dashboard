9ed926ec9cb2e954095c7c0c271232e42e80769e
Bobesz <bob@bobnet.network>
2020-09-19 16:52:52 +0200

    Update tag.service.ts

--- src/app/services/model/content/tag/tag.service.ts
+++ src/app/services/model/content/tag/tag.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Tag } from 'src/app/model/content/Tag';
+import { BackendConfigService } from 'src/app/services/tool/backendconfig/backendconfig.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TagService {
 
-  constructor() { }
+  private url = new BackendConfigService().getTagsUrl();
+
+  constructor(private http: HttpClient) { }
+
+  getAllTags() {
+    return this.http.get(this.url);
+  }
+
+  getTag(slug: string) {
+    return this.http.get(this.url + slug);
+  }
+
+  insertTag(tag: Tag) {
+    return this.http.post(this.url, tag);
+  }
+
+  updateTag(tag: Tag) {
+    return this.http.put(this.url, tag);
+  }
+
+  deleteTag(slug: string) {
+    return this.http.delete(this.url + slug);
+  }
 }
