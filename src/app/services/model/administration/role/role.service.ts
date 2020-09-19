bdb6b0ad66d9899501e1aea7cb74bb0848fb077b
Bobesz <bob@bobnet.network>
2020-09-19 16:56:55 +0200

    Update role.service.ts

--- src/app/services/model/administration/role/role.service.ts
+++ src/app/services/model/administration/role/role.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Role } from 'src/app/model/user/Role';
+import { BackendConfigService } from 'src/app/services/tool/backendconfig/backendconfig.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleService {
 
-  constructor() { }
+  private url = new BackendConfigService().getRolesUrl();
+
+  constructor(private http: HttpClient) { }
+
+  getAllRoles() {
+    return this.http.get(this.url);
+  }
+
+  getRole(slug: string) {
+    return this.http.get(this.url + slug);
+  }
+
+  insertRole(role: Role) {
+    return this.http.post(this.url, role);
+  }
+
+  updateRole(role: Role) {
+    return this.http.put(this.url, role);
+  }
+
+  deleteRole(slug: string) {
+    return this.http.delete(this.url + slug);
+  }
 }
