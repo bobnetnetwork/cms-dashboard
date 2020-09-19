f5d479ba5fe641883cb264d4edf2a9963f8a170c
Bobesz <bob@bobnet.network>
2020-09-19 17:05:26 +0200

    Update settings.service.ts

--- src/app/services/model/administration/settings/settings.service.ts
+++ src/app/services/model/administration/settings/settings.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BackendConfigService } from 'src/app/services/tool/backendconfig/backendconfig.service';
+import { Option } from 'src/app/model/administration/Option';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SettingsService {
 
-  constructor() { }
+  private url = new BackendConfigService().getSettingsUrl();
+
+  constructor(private http: HttpClient) { }
+
+  getAllOptions() {
+    return this.http.get(this.url);
+  }
+
+  getOption(slug: string) {
+    return this.http.get(this.url + slug);
+  }
+
+  insertOption(option: Option) {
+    return this.http.post(this.url, option);
+  }
+
+  updateOption(option: Option) {
+    return this.http.put(this.url, option);
+  }
+
+  deleteOption(slug: string) {
+    return this.http.delete(this.url + slug);
+  }
 }
