70139fa305f435cec81c8aa372b2aa4404610f81
Bobesz <bob@bobnet.network>
2020-09-19 16:50:12 +0200

    Update media.service.ts

--- src/app/services/model/content/media/media.service.ts
+++ src/app/services/model/content/media/media.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BackendConfigService } from 'src/app/services/tool/backendconfig/backendconfig.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MediaService {
 
-  constructor() { }
+  private url = new BackendConfigService().getMediaUrl();
+
+  constructor(private http: HttpClient) { }
+
+  getAllMedia() {
+    return this.http.get(this.url);
+  }
+
+  getMedia(slug: string) {
+    return this.http.get(this.url + slug);
+  }
+
+  insertMedia(media: any) {
+    return this.http.post(this.url, media);
+  }
+
+  updateMedia(media: any) {
+    return this.http.put(this.url, media);
+  }
+
+  deleteMedia(slug: string) {
+    return this.http.delete(this.url + slug);
+  }
 }
